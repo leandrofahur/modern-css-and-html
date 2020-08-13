1edbb8356a2851803e8cc7e23b6f8a42d2393bd3
leandrofahur <leandrofahur@gmail.com>
2020-08-12 22:58:20 -0700

    adding behaviour with jquery and js

---
+++ projects/edgeledger/js/handleNavbar.js
@@ -0,0 +1,29 @@
+const navbar = document.getElementById('navbar');
+let scrolled = false;
+
+window.onscroll = () => {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove('top');
+    if (!scrolled) {
+      navbar.style.transform = 'translateY(-70px)';
+    }
+    setTimeout(() => {
+      navbar.style.transform = 'translateY(0px)';
+      scrolled = true;
+    }, 200);
+  } else {
+    navbar.classList.add('top');
+    scrolled = false;
+  }
+}
+
+$('#navbar a, .btn').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top - 100,
+    }, 800);
+  }
+});
