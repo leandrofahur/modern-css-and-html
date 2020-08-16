acec98cf532973d70e47836f98932c78eccbba3a
leandrofahur <leandrofahur@gmail.com>
2020-08-16 01:30:59 -0700

    auto-fit for the grid + minmax(minValu, maxValue)

--- sandbox/memory-game/js/app.js
+++ sandbox/memory-game/js/app.js
@@ -1,6 +1,9 @@
 const memoryCard = document.querySelector('.memory-grid');
 
-const div = document.createElement('div');
-div.innerHTML = `<img src="../img/img1.jpeg" alt="">`;
-memoryCard.appendChild(div)
+for (let i = 0; i < 12; i++) {
+  const div = document.createElement('div');
+  div.innerHTML = `<img src="../img/img1.jpeg" alt="">`;
+  memoryCard.appendChild(div);
+}
+
 
