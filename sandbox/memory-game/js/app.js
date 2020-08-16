ff0c7c7bb08bc23f6e65000b1d32c3e673e07aa9
leandrofahur <leandrofahur@gmail.com>
2020-08-16 02:02:18 -0700

    create the grid with images

--- sandbox/memory-game/js/app.js
+++ sandbox/memory-game/js/app.js
@@ -1,9 +1,31 @@
 const memoryCard = document.querySelector('.memory-grid');
 
-for (let i = 0; i < 12; i++) {
+for (let i = 1; i < 10; i++) {
   const div = document.createElement('div');
-  div.innerHTML = `<img src="../img/img1.jpeg" alt="">`;
-  memoryCard.appendChild(div);
+  div.innerHTML = `<img src="../img/img${i}.jpg">`
+
+  memoryCard.appendChild(div)
 }
+
+
+
+
+
+
+
+// for (let i = 0; i < 24; i++) {
+//   fetch('https://randomuser.me/api/').then(data => {
+//     data.json().then(result => {
+//       profile = result.results[0].picture.medium;
+
+//       const div = document.createElement('div');
+//       div.innerHTML = `<img src="${profile}" alt="">`;
+//       memoryCard.appendChild(div);
+//     })
+//   });
+// }
+
+
+
 
 
