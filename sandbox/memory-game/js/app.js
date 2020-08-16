011321dee50fc7221246155eefef23a850220ff0
leandrofahur <leandrofahur@gmail.com>
2020-08-16 01:11:47 -0700

    started memory game

---
+++ sandbox/memory-game/js/app.js
@@ -0,0 +1,6 @@
+const memoryCard = document.querySelector('.memory-grid');
+
+const div = document.createElement('div');
+div.innerHTML = `<img src="../img/img1.jpeg" alt="">`;
+memoryCard.appendChild(div)
+
